359b5b82f6df6fee44249cdcbfc3cef0fd55b044
Sebastian Rios <seba.rios@gmail.com>
2016-04-24 20:46:09 -0300

    Fix test

--- test/api_spec.js
+++ test/api_spec.js
@@ -119,7 +119,7 @@ describe ('JSON Recipes', function(){
             res.body.should.be.an('object');
             res.body.data.should.be.an('array');
             res.body.data.length.should.be.equal(3);
-            res.body.data[0].title.should.be.equal('Título')
+            res.body.data[0].title.should.be.equal('IPAD MINI 16gb SILVER IMPECABLE CONDICION')
             done();
           });
       });
